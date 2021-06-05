12ceabf411bcded3b624b59c27ceab9f71899b28
pankaj11524 <47343113+pankaj11524@users.noreply.github.com>
2021-06-06 00:30:26 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,28 +1,46 @@
-module "web-server-launch" {
-  source = "../TerraformProject/modules/ec2"
-  ami = "ami-010aff33ed5991201"
-  #instance_type = "c4.xlarge" #If we are configuring cpu the type needs to increase
-  instance_type = "t2.micro"
-  instance_count = "1"
-  #key_name = ""  #If you are adding any param here then value needs to define else left if. default value to be pick from variable.tf
-  additional_tags = {
-    Name = "Web-Host"
-    Environment = "Dev"
-    Project = "Banking"
-    TechTeam = "Java-Development"
+resource "aws_instance" "web-application-server" {
+  ami = var.ami
+  instance_type = var.instance_type
+  count = var.instance_count
+  key_name = var.key_name
+  tags = {
+    Name = "${var.additional_tags.Name}-${count.index + 1}"
+    Environment = var.additional_tags.Environment
+    Project = var.additional_tags.Project
+    TechTeam = var.additional_tags.TechTeam
+      }
+  volume_tags = {
+    Name = var.additional_tags.Environment
   }
-  subnet_id = "subnet-37b5cc7b"
-  #vpc_security_group_ids = ["sg-08426877d0959bf8c"]
-  associate_public_ip_address = true
-  disable_api_termination = false
-  security_groups = ["sg-08426877d0959bf8c"]
-  #cpu_core_count = "2"
-  #cpu_threads_per_core = "1"
-  cpu_core_count = ""
-  cpu_threads_per_core = ""
-  user_data = <<EOF
-echo "This is the test string..."
-yum install httpd
-  EOF
-  volume_size = 50  #This value means 50GiB
+  subnet_id = var.subnet_id
+  #vpc_security_group_ids = var.vpc_security_group_ids      #List of associated security groups of instances, if running in non-default VPC
+  associate_public_ip_address = var.associate_public_ip_address
+  disable_api_termination = var.disable_api_termination
+  monitoring = true
+  security_groups = var.security_groups
+#  cpu_core_count = var.cpu_core_count #If you want to configure cpu count
+#  cpu_threads_per_core = var.cpu_threads_per_core ##If you want to configure cpu treads per core
+  user_data = var.user_data
+  root_block_device {
+    delete_on_termination = true
+    encrypted = false
+    iops = 100
+    kms_key_id = "" #(Optional) Amazon Resource Name (ARN) of the KMS Key to use when encrypting the volume. Must be configured to perform drift detection.
+  }
+}
+#below resources values to be declared in variable files
+resource "aws_ebs_volume" "dev-volume" {
+  availability_zone = aws_instance.web-application-server[count.index].availability_zone
+  count = 1
+  #count = var.instance_count #If want to create volume for all instances
+  size = var.volume_size
+  tags = {
+    Name = "dev-volume"
+  }
+  }
+resource "aws_volume_attachment" "dev-volume-attachment" {
+  device_name = "/dev/sdh"
+  instance_id = aws_instance.web-application-server[count.index].id
+  volume_id = aws_ebs_volume.dev-volume[count.index].id
+  count = 1
 }
